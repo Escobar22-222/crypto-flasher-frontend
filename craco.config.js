4a16e5902b7ded42bdc166e28ae230595576de85
Escobar22-222 <hillgodshop@gmail.com>
2025-04-17 00:39:53 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -5,14 +5,15 @@ module.exports = {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        buffer: require.resolve("buffer"), // ✅ FIXED
+        buffer: require.resolve("buffer/"),
         stream: require.resolve("stream-browserify"),
-        crypto: require.resolve("crypto-browserify")
+        crypto: require.resolve("crypto-browserify"),
+        process: require.resolve("process/browser.js") // ✅ FIXED: add extension
       };
       webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
-          process: "process/browser"
+          process: ["process"] // ✅ FIXED: array format
         })
       );
       return webpackConfig;
